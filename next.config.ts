3011c51eb34c50902a32b8850588b37965bc02c7
gaurav-vij16 <gaurav.21scse1010546@galgotiasuniversity.edu.in>
2026-01-23 00:03:00 +0530

    temporary fix to clear cached /lander redirect

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/lander",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
